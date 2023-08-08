3422d841d72034fdc4083df5f5a11b0d0e823b32
Scott Brown <scott_m_brown@mac.com>
2023-08-08 18:52:20 +0100

    Initial Commit

---
+++ bluCount/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  bluCount
+//
+//  Created by Scott Brown on 08/08/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ bluCount/bluCountApp.swift
@@ -0,0 +1,17 @@
+//
+//  bluCountApp.swift
+//  bluCount
+//
+//  Created by Scott Brown on 08/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct bluCountApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
